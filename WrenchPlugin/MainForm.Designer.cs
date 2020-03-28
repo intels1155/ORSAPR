29e358adad2d4d24da406ef42a14ba085655c349
intels1155 <intels1155@gmail.com>
2020-03-29 01:50:52 +0700

    Добавлены файлы проекта

---
+++ WrenchPlugin/MainForm.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace WindowsFormsApp2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.WrenchSize1Label = new System.Windows.Forms.Label();
+			this.WrenchSize2Label = new System.Windows.Forms.Label();
+			this.HexagonDepthLabel = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.StartKompasAppButton = new System.Windows.Forms.Button();
+			this.BuildWrenchButton = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// WrenchSize1Label
+			// 
+			this.WrenchSize1Label.AutoSize = true;
+			this.WrenchSize1Label.Location = new System.Drawing.Point(22, 51);
+			this.WrenchSize1Label.Name = "WrenchSize1Label";
+			this.WrenchSize1Label.Size = new System.Drawing.Size(116, 13);
+			this.WrenchSize1Label.TabIndex = 0;
+			this.WrenchSize1Label.Text = "Ширина зева ключа 1";
+			// 
+			// WrenchSize2Label
+			// 
+			this.WrenchSize2Label.AutoSize = true;
+			this.WrenchSize2Label.Location = new System.Drawing.Point(22, 88);
+			this.WrenchSize2Label.Name = "WrenchSize2Label";
+			this.WrenchSize2Label.Size = new System.Drawing.Size(116, 13);
+			this.WrenchSize2Label.TabIndex = 1;
+			this.WrenchSize2Label.Text = "Ширина зева ключа 2";
+			// 
+			// HexagonDepthLabel
+			// 
+			this.HexagonDepthLabel.AutoSize = true;
+			this.HexagonDepthLabel.Location = new System.Drawing.Point(22, 124);
+			this.HexagonDepthLabel.Name = "HexagonDepthLabel";
+			this.HexagonDepthLabel.Size = new System.Drawing.Size(84, 13);
+			this.HexagonDepthLabel.TabIndex = 2;
+			this.HexagonDepthLabel.Text = "Глубина зева 1";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(22, 159);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(84, 13);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "Глубина зева 2";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(22, 196);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(74, 13);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "Длина ключа";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(22, 233);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(125, 13);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "Толщина стенки ключа";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(22, 272);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(108, 13);
+			this.label7.TabIndex = 6;
+			this.label7.Text = "Диаметр отверстий";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(211, 48);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 8;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(211, 85);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 9;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(211, 121);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 10;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(211, 156);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 11;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(211, 193);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 12;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(211, 230);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 20);
+			this.textBox6.TabIndex = 13;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(211, 269);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(100, 20);
+			this.textBox7.TabIndex = 14;
+			// 
+			// StartKompasAppButton
+			// 
+			this.StartKompasAppButton.Location = new System.Drawing.Point(25, 318);
+			this.StartKompasAppButton.Name = "StartKompasAppButton";
+			this.StartKompasAppButton.Size = new System.Drawing.Size(123, 33);
+			this.StartKompasAppButton.TabIndex = 15;
+			this.StartKompasAppButton.Text = "Запуск КОМПАС-3D";
+			this.StartKompasAppButton.UseVisualStyleBackColor = true;
+			// 
+			// BuildWrenchButton
+			// 
+			this.BuildWrenchButton.Location = new System.Drawing.Point(188, 318);
+			this.BuildWrenchButton.Name = "BuildWrenchButton";
+			this.BuildWrenchButton.Size = new System.Drawing.Size(123, 33);
+			this.BuildWrenchButton.TabIndex = 16;
+			this.BuildWrenchButton.Text = "Построить";
+			this.BuildWrenchButton.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+			this.label1.Location = new System.Drawing.Point(13, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(233, 13);
+			this.label1.TabIndex = 17;
+			this.label1.Text = "Задайте параметры модели в миллиметрах.";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(337, 367);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.BuildWrenchButton);
+			this.Controls.Add(this.StartKompasAppButton);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.HexagonDepthLabel);
+			this.Controls.Add(this.WrenchSize2Label);
+			this.Controls.Add(this.WrenchSize1Label);
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.ShowIcon = false;
+			this.Text = "Торцевой ключ - КОМПАС-3D";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label WrenchSize1Label;
+		private System.Windows.Forms.Label WrenchSize2Label;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Button StartKompasAppButton;
+		private System.Windows.Forms.Button BuildWrenchButton;
+		private System.Windows.Forms.Label HexagonDepthLabel;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
